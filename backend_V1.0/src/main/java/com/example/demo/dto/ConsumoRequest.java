0d379e0c71cf139349d7049c4b26b9b3e44461a4
Gerald Villalobos <60934334+geraldgra@users.noreply.github.com>
2026-08-07 10:35:41 -0400

    Delete backend_V1.0 directory
    se eliminara para cargar la version definitiva

--- backend_V1.0/src/main/java/com/example/demo/dto/ConsumoRequest.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.dto;
-
-public record ConsumoRequest(
-        Double consumoKwh,
-        Boolean usoHorarioPico,
-        Integer cantidadEquipos,
-        String tipoInmueble,
-        Integer horasAltoConsumo
-) {}
